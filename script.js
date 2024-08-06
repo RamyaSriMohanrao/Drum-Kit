6be6ccdcc430ff09a14f2ddfbf73be2b0f69c8a2
RamyaSriMohanrao <ramyasrimohanrao@gmail.com>
2024-08-06 16:15:03 +0530

    created drum kit

---
+++ script.js
@@ -0,0 +1,21 @@
+function playSound(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    // console.log(audio);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return;
+    audio.currentTime = 0;
+
+    audio.play();
+    key.classList.add('playing');
+}
+    
+
+function removeTransition(e){
+    if(e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+console.log(keys,'keyslog')
+keys.forEach(key => key.addEventListener('transitionend',removeTransition));
+window.addEventListener('keydown',playSound); 
